bb24edc4928c8550bbb56f4d5bbec7e61378382d
Lucas Walter <wsacul@gmail.com>
2020-08-01 20:44:41 -0700

    Trees and lampposts, the grid map has collisions now- need to get parenting right in tiles.tscn so collisions work.  Also deleting items out of meshlib is confusing to the gridmap, have to start over when that happens.

--- afterblade/player.gd
+++ afterblade/player.gd
@@ -24,5 +24,10 @@ func _physics_process(delta):
         vel.x -= acc * 0.2
     if Input.is_action_pressed('ui_right'):
         vel.x += acc * 0.2
+    if Input.is_action_just_pressed('ui_select'):
+        vel.y += 5.0 * (1.0 + abs(vel.x))
+    # TODO(lucasw) can't keep accelerating forever, need to detect
+    # down collision and zero out vel.y
+    vel.y -= 0.1
     move_and_slide(vel)
     vel *= 0.96
